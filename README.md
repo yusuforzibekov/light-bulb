0776fb5c4faa674f94991e63b8ae800e16e3065b
Orzibekov Yusufbek <orzibekovyusufbek@gmail.com>
2023-10-22 15:33:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### JS practice 
+Hi guys check this out.
---
+++ js/script.js
@@ -0,0 +1,7 @@
+function turnOn() {
+    document.getElementById('img').src = '/images/photo_2023-10-22_14-59-15.jpg';
+}
+
+function turnOff() {
+    document.getElementById('img').src = '/images/photo_2023-10-22_14-59-17.jpg';
+}
